2ebbc20094a9a6f609fe432dd8e1015c2f836dc5
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2022-12-01 12:41:59 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>repl.it</title>
+    <link href="./css/global.css" rel="stylesheet" type="text/css" />
+    <link href="./css/slider_flex.css" rel="stylesheet" type="text/css" />  
+  	</head>
+  <body>
+    <div class="flex_slider">
+      <div class="item item1">
+        <span>Eren</span>
+      </div>
+      <div class="item item2">
+        <span>Mikasa</span>
+      </div>
+      <div class="item item3">
+        <span>Armin</span>
+      </div>
+      <div class="item item4">
+        <span>Jan</span>
+      </div>
+    </div>
+    
+    <script src="script.js"></script>
+  </body>
+</html>
